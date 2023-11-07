aa532200da3c2f99116336c74d2e2487bf7481f7
saksham soni <144764828+sakshamwebmitr@users.noreply.github.com>
2023-11-07 11:27:22 +0530

    second

--- src/components/Mainnews.jsx
+++ src/components/Mainnews.jsx
@@ -2,15 +2,99 @@
 
 const Mainnews = () => {
   return (
-    <div className='w-full h-[150vh] bg-slate-500 flex flex-col items-center justify-evenly' >
-        <div className="w-11/12 h-3/4 bg-slate-600 flex flex-row items-center justify-evenly" >
-            <div className="w-4/6 h-full bg-slate-200 flex flex-row items-center justify-evenly" >
-                <div className="w-5/12 h-full bg-slate-800" ></div>
-                <div className="w-5/12 h-full bg-slate-950" ></div>
+    <div className='w-full h-[150vh] flex flex-col items-center justify-evenly' >
+        <div className="w-11/12 h-3/4  flex flex-row items-center justify-evenly" >
+            <div className="w-4/6 h-full  flex flex-row items-center justify-evenly" >
+                <div className="w-5/12 h-full  flex flex-col items-center justify-evenly" > 
+                <a href="" className="w-full h-2/3  flex flex-col justify-evenly items-center" >
+                    <div className="w-full h-32 flex items-center justify-center" ><h1 className="font-bold text-2xl" >
+                    दिल्ली-NCR में महसूस किए गए भूकंप के तेज झटके, 5.6 रही तीव्रता, चार दिन में दूसरी बार हिली धरती</h1></div>
+                    <div className="w-full h-64 " ><img src="https://images.pexels.com/photos/2079223/pexels-photo-2079223.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" /></div>
+                    <div className="w-full h-24" ><h5>दिल्ली-NCR में भूकंप के तेज झटके महसूस किए गए हैं. रिक्टर स्केल पर इस भूकंप की तीव्रता 5.6 मापी गई. जानकारी के मुताबिक इस भूकंप का केंद्र भी नेपाल ही था.</h5></div>
+                </a>
+                <a href="" className="w-full h-2/6 flex flex-col items-center justify-evenly" >
+                    <div className="w-full h-28 flex items-center justify-center"> <h1 className="font-bold text-2xl" >वर्ल्ड कप में बवाल... 146 साल के क्रिकेट इतिहास में पहली बार टाइमआउट हुआ ये बल्लेबाज</h1></div>
+                    <div className="w-full h-32  flex flex-row items-center justify-evenly">
+                        <div className="w-40 h-28 " ><img className="w-full h-full" src="https://images.pexels.com/photos/15862398/pexels-photo-15862398/free-photo-of-indian-flag-at-cricket-ground.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" /></div>
+                        <div className="w-56 h-full  flex items-center justify-center px-2" ><h5>भारत की मेजबानी में खेले जा रहे वनडे वर्ल्ड कप 2023 में रोमांचक मुकाबलों के साथ कई विवाद भी सामने आए हैं. इसी बीच एक...</h5></div>
+                    </div>
+                </a>
+                </div>
+                <div className="w-5/12 h-full flex flex-col items-center justify-evenly" >
+                    <div className="w-full h-2/6 bg-slate-600" ></div>
+                    <div className="w-full h-4/6 flex flex-col items-center justify-evenly" >
+                        <a href="" className="w-full h-24 flex flex-row items-center justify-evenly" >
+                            <div className="w-40 h-20  object-cover object-center overflow-hidden" ><img className="w-full h-full" src="https://www.oneindia.com/img/2019/01/yogi-adityanath-1547871855.jpg" alt="" /></div>
+                            <div className="w-56 h-24 flex items-center justify-center" ><h4 className="font-bold" >दिवाली से पहले योगी सरकार का बड़ा ऐलान... कर्मचारियों को 46% DA के साथ बोनस का तोहफा</h4></div>
+                        </a>
+                        <a href="" className="w-full h-24 flex flex-row items-center justify-evenly" >
+                            <div className="w-40 h-20  object-cover object-center overflow-hidden" ><img className="w-full h-full" src="https://www.oneindia.com/img/2019/01/yogi-adityanath-1547871855.jpg" alt="" /></div>
+                            <div className="w-56 h-24 flex items-center justify-center" ><h4 className="font-bold" >दिवाली से पहले योगी सरकार का बड़ा ऐलान... कर्मचारियों को 46% DA के साथ बोनस का तोहफा</h4></div>
+                        </a>
+                        <a href="" className="w-full h-24 flex flex-row items-center justify-evenly" >
+                            <div className="w-40 h-20  object-cover object-center overflow-hidden" ><img className="w-full h-full" src="https://www.oneindia.com/img/2019/01/yogi-adityanath-1547871855.jpg" alt="" /></div>
+                            <div className="w-56 h-24 flex items-center justify-center" ><h4 className="font-bold" >दिवाली से पहले योगी सरकार का बड़ा ऐलान... कर्मचारियों को 46% DA के साथ बोनस का तोहफा</h4></div>
+                        </a>
+                        <a href="" className="w-full h-24 flex flex-row items-center justify-evenly" >
+                            <div className="w-40 h-20  object-cover object-center overflow-hidden" ><img className="w-full h-full" src="https://www.oneindia.com/img/2019/01/yogi-adityanath-1547871855.jpg" alt="" /></div>
+                            <div className="w-56 h-24 flex items-center justify-center" ><h4 className="font-bold" >दिवाली से पहले योगी सरकार का बड़ा ऐलान... कर्मचारियों को 46% DA के साथ बोनस का तोहफा</h4></div>
+                        </a>
+                        <a href="" className="w-full h-24 flex flex-row items-center justify-evenly" >
+                            <div className="w-40 h-20  object-cover object-center overflow-hidden" ><img className="w-full h-full" src="https://www.oneindia.com/img/2019/01/yogi-adityanath-1547871855.jpg" alt="" /></div>
+                            <div className="w-56 h-24 flex items-center justify-center" ><h4 className="font-bold" >दिवाली से पहले योगी सरकार का बड़ा ऐलान... कर्मचारियों को 46% DA के साथ बोनस का तोहफा </h4></div>
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div className="w-4/12 h-full bg-slate-400" ></div>
+            <div className="w-4/12 h-full flex flex-row items-center justify-evenly" >
+                <div className="w-2/5 h-full flex flex-col items-center justify-evenly" >
+                    <div className="w-full h-24 bg-slate-300"></div>
+                    <div className="w-full h-24 bg-slate-300"></div>
+                    <div className="w-full h-24 bg-slate-300"></div>
+                    <div className="w-full h-24 bg-slate-300"></div>
+                    <div className="w-full h-24 bg-slate-300"></div>
+                    <div className="w-full h-24 bg-slate-300"></div>
+                    <div className="w-full h-24 bg-slate-300"></div>
+                    <div className="w-full h-24 bg-slate-300"></div>
+                </div>
+                <div className="w-3/5 h-full flex flex-col items-center justify-evenly" >
+                    <div className="w-full h-2/6 bg-slate-700" ></div>
+                    <div className="w-full h-3/5 flex flex-col items-center justify-evenly" >
+                        <div className="w-full h-1/3 bg-slate-500" ></div>
+                        <div className="w-full h-2/4 bg-slate-500" ></div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="w-11/12 h-1/4 bg-slate-300" ></div>
+        <div className="w-11/12 h-1/4 flex flex-row items-center justify-evenly" >
+            <div className="w-2/6 h-full bg-slate-500 flex flex-col items-center justify-evenly">
+            <div className="w-full h-9 bg-slate-200" ></div>
+            <div className="w-full h-16 bg-slate-300" ></div>
+            <div className="w-full h-16 bg-slate-400" ></div>
+            <div className="w-full h-16 bg-slate-500" ></div>
+            <div className="w-full h-16 bg-slate-600" ></div>
+            </div>
+            <div className="w-2/5 h-full  flex flex-row items-center justify-evenly">
+                <div className="w-1/4 h-5/6 flex flex-col items-center justify-evenly" >
+                    <div className="w-full h-2/5 bg-slate-500" ></div>
+                    <div className="w-full h-3/5 bg-slate-300" ></div>
+                </div>
+                <div className="w-1/4 h-5/6 flex flex-col items-center justify-evenly" >
+                    <div className="w-full h-2/5 bg-slate-500" ></div>
+                    <div className="w-full h-3/5 bg-slate-300" ></div>
+                </div>
+                <div className="w-1/4 h-5/6 flex flex-col items-center justify-evenly" >
+                    <div className="w-full h-2/5 bg-slate-500" ></div>
+                    <div className="w-full h-3/5 bg-slate-300" ></div>
+                </div>
+            </div>
+            <div className="w-3/12 h-full bg-slate-500 overflow-auto flex flex-col items-center justify-evenly">
+            <div className="w-full h-9 bg-slate-200" ></div>
+            <div className="w-full h-20 bg-slate-200" ></div>
+            <div className="w-full h-20 bg-slate-200" ></div>
+
+            </div>
+        </div>
     </div>
   )
 }
